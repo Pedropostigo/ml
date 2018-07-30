47337d15429cae1c0bb6ce9025c18a53e168cf3e
Pedro <postigo.laguna@gmail.com>
2018-07-30 12:58:31 +0200

    Added XGBRegressor parameters

--- xgboost.py
+++ xgboost.py
@@ -80,10 +80,37 @@ class XGBClassifier(XGBClass):
 
 # class for the CGB Regressor where the methods will be added
 class XGBRegressor(XGBReg):
-    # TODO: cambiar el init method para que tenga todos los parámetros que toca
-    def __init__():
+    def __init__(self,
+                max_depth = 3,
+                learning_rate = 0.1,
+                n_estimators = 100,
+                silent = True,
+                objective ='reg:linear',
+                booster = 'gbtree',
+                n_jobs = 1,
+                nthread = None,
+                gamma = 0,
+                min_child_weight = 1,
+                max_delta_step = 0,
+                subsample = 1,
+                colsample_bytree = 1,
+                colsample_bylevel = 1,
+                reg_alpha = 0,
+                reg_lambda = 1,
+                scale_pos_weight = 1,
+                base_score = 0.5,
+                random_state = 0,
+                seed = None,
+                missing = None,
+                **kwargs):
         # call the original __init__ method of the XGBClassifier
-        super(XGBRegressor, self).__init__()
+        super(XGBRegressor, self).__init__(max_depth, learning_rate, n_estimators,
+                                            silent, objective, booster, n_jobs,
+                                            nthread, gamma, min_child_weight,
+                                            max_delta_step, subsample, colsample_bytree,
+                                            colsample_bylevel, reg_alpha, reg_lambda,
+                                            scale_pos_weight, base_score, random_state,
+                                            seed, missing, **kwargs)
 
         self.isTrained = False
 
