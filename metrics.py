c3178adada1861e02f4e37d727d694855f7f90db
Pedropostigo <postigo.laguna@gmail.com>
2019-05-01 15:55:13 +0200

    added roc auc metric

--- metrics.py
+++ metrics.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.metrics import make_scorer, mean_squared_error, roc_auc_score
 
 # Root mean squared error
 def rmse(real, pred):
@@ -9,6 +9,10 @@ def rmse(real, pred):
 # Symmetric mean absolute percentage error
 def smape(real, pred):
     return (100 / len(real) * np.sum(np.abs(pred-real)/((np.abs(real)+np.abs(pred))/2)))
+
+# Area under the ROC curve
+def auc(real, pred):
+    return roc_auc_score(real, pred)
 
 def score(metric):
     """
