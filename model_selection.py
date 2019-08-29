9f2c25471a6bd9011bb9f055d53e875126b44e84
Pedro <postigo.laguna@gmail.com>
2019-08-30 00:13:04 +0200

    implemented function to tune models

--- model_selection.py
+++ model_selection.py
@@ -1,3 +1,12 @@
+"""
+module that has diferent functions to test and optimize models
+
+expand_grid -- function to return the combinations of parameters to test a model
+cv          -- function to cross validate a model
+copy_model  -- return an object of the same class as the input model
+tune_model  -- function to tune hyperparameters of a model given a grid of parameters 
+"""
+
 from itertools import product
 from time import time
 
@@ -5,7 +14,10 @@ from sklearn.model_selection import KFold, StratifiedKFold, TimeSeriesSplit
 
 import numpy as np
 
+import lightgbm as lgbm
 import catboost as cat
+
+from .handle_json import read_json, save_json
 
 
 def expand_grid(params):
@@ -102,7 +114,9 @@ def cv(model, X, y, metric, folds = 5, split_type = 'normal', seed = None, verbo
             model.fit(X.loc[id_train], y[id_train])
 
         # predict with the out-of-sample fold
-        preds[id_val] =  model.predict(X.loc[id_val])
+
+        # TODO: gran parche para que funcione con el LightGBM, arreglar cuanto antes
+        preds[id_val] =  [i[1] for i in model.predict_proba(X.loc[id_val])]
         # compute the metric and append it to the metrics list
         metrics.append(metric(y[id_val], preds[id_val]))
 
@@ -112,6 +126,58 @@ def cv(model, X, y, metric, folds = 5, split_type = 'normal', seed = None, verbo
             toc = time()
             print(f"Finished fold {i} of {folds} folds. Result: {metric(y[id_val], preds[id_val])}. " +
             f"Time taken: {np.round((toc - tic)/60, 1)} m.")
+            print("\n")
 
-    print("\n")
     return metrics, preds
+
+
+def copy_model(model):
+    """
+    Check the type of model that is given as an imput, and return a new object of the 
+    same class as the original model
+    """
+
+    if isinstance(model, lgbm.LGBMClassifier):
+        return lgbm.LGBMClassifier(**model.get_params())
+
+    else:
+        print("Copy model not implemented yet")
+
+def tune_model(model, X, y, params, metric, folds = 5, random_state = None, file_name = None):
+    
+    # get a tunning model
+    tunning_model = copy_model(model)
+    
+    # get the grid of parameters to tune the model
+    tune_params = expand_grid(params)
+    
+    # each tested params is a dictionary with the following structure {'params': {}, 'score': []}
+    if file_name is not None:
+        try:
+            tested_params = read_json(file_name)
+        except:
+            tested_params = {'tested_params': []}
+    else:
+        tested_params = {'tested_params': []}
+    
+    # tune the model with differenct params
+    for param in tune_params:
+        tunning_model.set_params(**param)
+        
+        # check if the params are already tested
+        tested_result = [result for result in tested_params['tested_params'] if result['params'] == tunning_model.get_params()]
+        if len(tested_result) == 1:
+            metrics = tested_result[0]['score']
+        else:
+            # TODO: no siempre tiene que ser stratified, ponerlo como parámetro
+            metrics, _ = cv(tunning_model, X, y, metric, folds = 5, split_type = 'stratified', seed = random_state)
+            tested_params['tested_params'].append({'params': tunning_model.get_params(),
+                                                   'score': metrics})
+            
+            # dump tested params into json file
+            if file_name is not None:
+                save_json(tested_params, file_name)
+
+        # TODO: la función debería retornar los resultados para cada parámetro, no hacer print de nada
+        print(f"Parameters: {param} | Score: {np.round(np.mean(metrics), 4)} +- {np.round(np.std(metrics), 5)}")
+    
