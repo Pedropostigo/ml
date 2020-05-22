8eace94ee7bf5d2e1c9d694d7b5c27ae9d0f292a
Pedropostigo <postigo.laguna@gmail.com>
2020-05-22 20:16:01 +0200

    added two parameters to control the outliers

--- feature_engineering/histogram.py
+++ feature_engineering/histogram.py
@@ -1,15 +1,19 @@
+import numpy as np
 import pandas as pd
 
-def histogram(X, feat, n_buckets = 10, scale = False, agg_params = None):
+def histogram(X, feat, n_buckets = 10, scale = False, left_outliers = None, 
+                right_outliers = None, agg_params = None):
     """
     Function to compute the histogram of a variable, and different agregation meassures for the rest of the columns
     
     Parameters
     -----------------
-    X (DataFrame)      --- pandas DataFrame containing the data to plot the histogram
+    X (DataFrame)       --- pandas DataFrame containing the data to plot the histogram
     feat (str)         --- name of the feature to compute the histogram
     n_buckets (int)    --- number of bins of the histrogram
     scale (bool)       --- whether to scale the values so the sum of values equals to 1
+    left_outliers (float) --- percentile threshold to consider outliers
+    right_outliers (float) --- percentile threshold to consider outliers
     agg_params (dict)  --- dictionary containing the features as keys, and a list of aggregations as values
     
     Returns
@@ -34,6 +38,30 @@ def histogram(X, feat, n_buckets = 10, scale = False, agg_params = None):
                 
     else:
         hist.loc[:, 'bin'] = pd.cut(hist[feat], bins = n_buckets, labels = False, duplicates = 'drop') + 1
+
+        if left_outliers is not None and right_outliers is not None:
+            subset = np.logical_and(hist[feat] >= hist[feat].quantile(left_outliers),
+                    hist[feat] <= hist[feat].quantile(right_outliers))
+
+            hist.loc[subset, 'bin'] = pd.cut(hist.loc[subset, feat], bins = n_buckets, labels = False, duplicates = 'drop') + 1
+            hist.loc[hist[feat] < hist[feat].quantile(left_outliers), 'bin'] = 0
+            hist.loc[hist[feat] > hist[feat].quantile(right_outliers), 'bin'] = n_buckets + 1
+
+        elif left_outliers is not None:
+            subset = hist[feat] >= hist[feat].quantile(left_outliers)
+
+            hist.loc[subset, 'bin'] = pd.cut(hist.loc[subset, feat], bins = n_buckets, labels = False, duplicates = 'drop') + 1
+            hist.loc[hist[feat] < hist[feat].quantile(right_outliers), 'bin'] = 0
+
+        elif right_outliers is not None:
+            subset = hist[feat] <= hist[feat].quantile(right_outliers)
+
+            hist.loc[subset, 'bin'] = pd.cut(hist.loc[subset, feat], bins = n_buckets, labels = False, duplicates = 'drop') + 1
+            hist.loc[hist[feat] > hist[feat].quantile(right_outliers), 'bin'] = n_buckets + 1
+
+        else:
+            hist['bin'] = pd.cut(hist[feat], bins = n_buckets, labels = False, duplicates = 'drop') + 1
+
         hist = hist.groupby(['bin'], as_index = False).agg(agg_dict)
         hist.columns = list(map('_'.join, hist.columns.values))
         hist = hist.reset_index(drop = True)
